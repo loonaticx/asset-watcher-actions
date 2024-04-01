0288e3b0c7d8397fc76046aa13215a01e2978767
Erica M. ("Loonatic") <l.oony@aol.com>
2024-04-01 18:49:20 -0500

    Update check_audio.py

--- check_audio.py
+++ check_audio.py
@@ -3,14 +3,9 @@ import os
 import mutagen
 from mutagen import FileType
 
-args = sys.argv
-print(f"Arrrrghs = {args}")
-fileList = []
-for argVal in args:
-    if argVal.endswith(".ogg"):
-        fileList.append(argVal)
 
-# argFiles
+# Todo: only checking newly modified files
+
 def get_all_oggs():
     file_list = []
     for dirpath, _, filenames in os.walk(os.getcwd()):
@@ -19,35 +14,20 @@ def get_all_oggs():
                 file_list.append(os.path.abspath(os.path.join(dirpath, file)))
     return file_list
 
-#musicFile = mutagen.File(f'sample/tt_s_ara_dga_trashcan_firstMoveLidFlip3.ogg')
-#musicFile = "sample/tt_s_ara_dga_trashcan_firstMoveLidFlip3.ogg"
-outputFilepath = os.environ.get("GITHUB_OUTPUT")
-if outputFilepath:
-    with open(outputFilepath) as coolFile:
-        print(f"cool ~ {coolFile.readlines()}")
-# print(f"GITHUB_OUTPUT = {newOggs}")
-if fileList:
-    print("Checking changed files!!")
-    oggs = newOggs.split("|")
-else:
-    oggs = get_all_oggs()
+
+oggs = get_all_oggs()
 
 formattedFiles = []
 for oggFile in oggs:
     print(f"--- {oggFile} ---")
     mFile = mutagen.File(oggFile)
-    mFile.tags = None
     if mFile.tags:
         print("Has tags!")
         sys.exit(1)
+    # We will have an option for this later if we wanna figure out how to give proper push perms :p
+    mFile.tags = None
     # FileType.save(mFile)
-    #mFile.save()
-    #formattedFiles.append(mFile)
+    # mFile.save()
+    # formattedFiles.append(mFile)
     print(mFile.pprint())
     print()
-
-# for formatFile in formattedFiles:
-#     print(f"Saving file {formatFile.info}")
-#     formatFile.save()
-
-# print(musicFile.pprint())
